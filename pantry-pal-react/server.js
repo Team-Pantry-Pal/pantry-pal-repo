6f09e68230f990667e9fc2684c0154cc17f24ebc
grantpant <grantpant@gmail.com>
2018-09-03 17:41:02 -0500

    The Realignment

--- pantry-pal-react/server.js
+++ pantry-pal-react/server.js
@@ -32,43 +32,43 @@ app.use('/api/pantry', pantryRouter);
 const port = process.env.PORT || 5000;
 // "process.env.PORT" is for when we deploy to Heroku
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log(`Server started on port ${port}...`);
 });
 
 
-//code for passport local 
-const passport = require("passport"); 
+//code for passport local
+const passport = require("passport");
 const LocalStrategy = require("passport-local")
-const passportLocalMongoose = require("passport-local-mongoose"); 
-//bring in user schema 
-const User = require("./models/user"); 
+const passportLocalMongoose = require("passport-local-mongoose");
+//bring in user schema
+const User = require("./models/user");
 
 app.use(require("express-session")({
-  secret: "blah blah blah", 
-  resave: false, 
+  secret: "blah blah blah",
+  resave: false,
   saveUninitialized: false
-})); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
-passport.use(new LocalStrategy(User.authenticate())); 
-//store user id in session 
-passport.serializeUser(User.serializeUser()); 
-//grab user object from database 
-passport.deserializeUser(User.deserializeUser()); 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+//store user id in session
+passport.serializeUser(User.serializeUser());
+//grab user object from database
+passport.deserializeUser(User.deserializeUser());
 
-//connect to mongo and set up body parser 
-mongoose.connect("mongodb://localhost/passport_demo"); 
-app.use(bodyParser.urlencoded({extended: true})); 
+//connect to mongo and set up body parser
+mongoose.connect("mongodb://localhost/passport_demo");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.render("home.ejs"); 
-}); 
+  res.render("home.ejs");
+});
 
 //route to signup page
 app.get("/signup", function(req, res){
-  res.render("signup.ejs"); 
-}); 
+  res.render("signup.ejs");
+});
 
 //islogged in
 function isLoggedIn(req, res, next){
@@ -83,12 +83,12 @@ app.get("/login", function(req, res) {
 	res.render("login.ejs");
 });
 
-//Should not be able to see newsfeed unless user is logged in 
+//Should not be able to see newsfeed unless user is logged in
 app.get("/newsfeed", isLoggedIn, function (req, res){
-  res.render("newsfeed.ejs"); 
-}); 
+  res.render("newsfeed.ejs");
+});
 
-//Logic for registering a user 
+//Logic for registering a user
 app.post("/signup", function(req, res) {
   var newUser = new User({username: req.body.username});
 	User.register(newUser, req.body.password, function(err, user){
@@ -104,14 +104,14 @@ app.post("/signup", function(req, res) {
 });
 
 //login logic
-app.post('/login', passport.authenticate('local', 
+app.post('/login', passport.authenticate('local',
 	{
 		successRedirect: '/newsfeed',
 		failureRedirect: '/login'
 	}), function(req, res){
 });
 
-//logout logic 
+//logout logic
 app.get('/logout', function(req, res){
 	req.logout();
 	res.redirect('/');
