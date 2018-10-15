0118b2bae8c5d658b8d90438d513117aa0649148
Curtis Adams <curt.b.adams@gmail.com>
2018-10-14 20:48:50 -0500

    Updated modal on recipe search to display more information

--- pantry-pal-react/client/src/components/FavRecipes.js
+++ pantry-pal-react/client/src/components/FavRecipes.js
@@ -1,6 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import DashNav from "./DashNav";
-import { Container, Card, Button, CardImg, CardTitle, CardText, CardDeck, CardSubtitle, CardBody, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+  Container,
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
 
 class FavRecipes extends Component {
   state = {
@@ -12,37 +26,37 @@ class FavRecipes extends Component {
   componentDidMount() {
     let userPayload = { user: this.props.user };
 
-    fetch('api/fav-recipes', {
-      method: 'POST',
+    fetch("api/fav-recipes", {
+      method: "POST",
       body: JSON.stringify(userPayload),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" }
     })
-    .then(res => res.json())
-    .then(data => {
-      let favRecipes = data.favRecipes;
-      this.setState({ favRecipes });
-    })
-    .catch(err => console.log(err));
+      .then(res => res.json())
+      .then(data => {
+        let favRecipes = data.favRecipes;
+        this.setState({ favRecipes });
+      })
+      .catch(err => console.log(err));
   }
 
-  deleteFav = (_id) => {
+  deleteFav = _id => {
     // code to delete fav
-  }
+  };
 
-  viewDetails = (_id) => {
+  viewDetails = _id => {
     let recipes = this.state.favRecipes;
-    const matchId = (recipe) => {
+    const matchId = recipe => {
       return recipe._id === _id;
-    }
+    };
     let favDetails = recipes.find(matchId);
 
     this.setState({ favDetails });
     this.toggle();
-  }
+  };
 
   toggle = () => {
     this.setState({ modal: !this.state.modal });
-  }
+  };
 
   render() {
     const favRecipes = this.state.favRecipes;
@@ -57,58 +71,66 @@ class FavRecipes extends Component {
 
     return (
       <div className="fav-recipes">
-        <DashNav
-          user={this.props.user}
-          logOutUser={this.props.logOutUser}
-        />
+        <DashNav user={this.props.user} logOutUser={this.props.logOutUser} />
         <Container>
           <CardDeck>
-            {favRecipes.map(({_id, image, title}) => (
+            {favRecipes.map(({ _id, image, title }) => (
               <Card key={_id}>
-              <CardImg top width="100%" src={image} alt="Card image cap" />
-              <CardBody>
-                <CardTitle>{title}</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                <Button onClick={this.viewDetails.bind(this, _id)}>View Details</Button>
+                <CardImg top width="100%" src={image} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>{title}</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </CardText>
+                  <Button onClick={this.viewDetails.bind(this, _id)}>
+                    View Details
+                  </Button>
 
-                <Button
-                  className="remove-btn"
-                  color="danger"
-                  size="sm"
-                  style={{float: "right"}}
-                  onClick={this.deleteFav.bind(this, _id)}
-                >
-                  &times;
-                </Button>
-              </CardBody>
-            </Card>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    style={{ float: "right" }}
+                    onClick={this.deleteFav.bind(this, _id)}
+                  >
+                    &times;
+                  </Button>
+                </CardBody>
+              </Card>
             ))}
           </CardDeck>
           <Modal isOpen={this.state.modal} toggle={this.toggle}>
-            <ModalHeader toggle={this.toggle}></ModalHeader>
+            <ModalHeader toggle={this.toggle} />
             <ModalBody>
               <Card>
-                <CardImg top width='100%' src={favDetails.image} alt={favDetails.title} />
+                <CardImg
+                  top
+                  width="100%"
+                  src={favDetails.image}
+                  alt={favDetails.title}
+                />
                 <CardBody>
                   <CardTitle>{favDetails.title}</CardTitle>
-                    <h6>Ingredients:</h6>
-                    <ul style={{listStyleType: 'none'}}>
-                      {ingredients.map(({id, name}) => (
-                        <li key={id} style={{listStyleType: "square"}}>{name}</li>
-                      ))}
-                    </ul>
-                    <h6>Instructions:</h6>
-                    <p>{favDetails.instructions}</p>
+                  <h6>Ingredients:</h6>
+                  <ul style={{ listStyleType: "none" }}>
+                    {ingredients.map(({ id, name }) => (
+                      <li key={id} style={{ listStyleType: "square" }}>
+                        {name}
+                      </li>
+                    ))}
+                  </ul>
+                  <h6>Instructions:</h6>
+                  <p>{favDetails.instructions}</p>
                 </CardBody>
               </Card>
             </ModalBody>
-            <ModalFooter>
-            </ModalFooter>
+            <ModalFooter />
           </Modal>
         </Container>
       </div>
-
     );
   }
 }
--- pantry-pal-react/client/src/components/RecipeSearch.js
+++ pantry-pal-react/client/src/components/RecipeSearch.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 import DashNav from "./DashNav";
-import { Container, Form, FormGroup, Label, Input, Button, CardDeck, Card, CardImg, CardBody, CardTitle, Jumbotron } from 'reactstrap';
+import {
+  Container,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  CardDeck,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  Jumbotron
+} from "reactstrap";
 //imports for modal
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
 
 class RecipeSearch extends Component {
-
   state = {
     searchResults: [],
-    value: '',
+    value: "",
     recipeDetails: {}
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({ value: event.target.value });
   };
 
   findRecipes = (event, err) => {
     event.preventDefault();
-    let searchData = { ingredients: this.state.value};
+    let searchData = { ingredients: this.state.value };
     // console.log(searchData);
-    fetch('api/recipe-search', {
-      method: 'POST',
+    fetch("api/recipe-search", {
+      method: "POST",
       body: JSON.stringify(searchData),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" }
     })
       .then(function(res) {
         let shit = res.json();
@@ -33,16 +45,16 @@ class RecipeSearch extends Component {
         this.setState({ searchResults: results });
       })
       .catch(err);
-    this.setState({ value: '' });
+    this.setState({ value: "" });
   };
-//second api request to get recipe details
+  //second api request to get recipe details
   recipeDetails = (id, event, err) => {
     event.preventDefault();
     let recipeId = { idNumber: id };
-    fetch('api/recipe-search/recipedetails', {
-      method: 'POST',
+    fetch("api/recipe-search/recipedetails", {
+      method: "POST",
       body: JSON.stringify(recipeId),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" }
     })
       .then(res => res.json())
       .then(result => {
@@ -52,7 +64,7 @@ class RecipeSearch extends Component {
         }
       })
       .catch(err);
-      this.setState({ ID: '' });
+    this.setState({ ID: "" });
   };
 
   addToFavs = () => {
@@ -69,17 +81,17 @@ class RecipeSearch extends Component {
         instructions: recipeDetails.instructions
       }
     };
-    fetch('api/fav-recipes/addfav', {
-      method: 'POST',
+    fetch("api/fav-recipes/addfav", {
+      method: "POST",
       body: JSON.stringify(favPayload),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" }
     })
-    .then(res => res.json())
-    .then(success => {
-      // show user confirmation
-      console.log(success);
-    })
-    .catch(err => console.log(err.message));
+      .then(res => res.json())
+      .then(success => {
+        // show user confirmation
+        console.log(success);
+      })
+      .catch(err => console.log(err.message));
   };
 
   toggle = () => {
@@ -89,23 +101,25 @@ class RecipeSearch extends Component {
   render() {
     const { searchResults } = this.state;
     const { recipeDetails } = this.state;
-    let aisle;
+    let ingredients;
     if (this.state.recipeDetails.extendedIngredients) {
-      aisle = recipeDetails.extendedIngredients[0].aisle;
+      ingredients = this.state.recipeDetails.extendedIngredients;
+      console.log(ingredients);
+    } else {
+      ingredients = [];
     }
 
     return (
       <div className="search-results">
-        <DashNav
-          user={this.props.user}
-          logOutUser={this.props.logOutUser}
-        />
+        <DashNav user={this.props.user} logOutUser={this.props.logOutUser} />
         <Container fluid={true}>
           <Jumbotron>
             <Form onSubmit={this.findRecipes}>
               <FormGroup>
                 <h3>Search by Ingredient</h3>
-                <Label for="recipeSearch">(Separate multiple ingredients with a comma)</Label>
+                <Label for="recipeSearch">
+                  (Separate multiple ingredients with a comma)
+                </Label>
                 <Input
                   type="search"
                   name="search"
@@ -117,27 +131,48 @@ class RecipeSearch extends Component {
               </FormGroup>
               <Button>Submit</Button>
             </Form>
-              <br />
+            <br />
             <CardDeck>
-              {searchResults.map(({ id, title, image }
-              ) => (
+              {searchResults.map(({ id, title, image }) => (
                 <Card key={id}>
                   <CardImg top width="100%" src={image} />
                   <CardBody>
                     <CardTitle>{title}</CardTitle>
                   </CardBody>
-                  <Button onClick={this.recipeDetails.bind(this, id)}>View Recipe</Button>
+                  <Button onClick={this.recipeDetails.bind(this, id)}>
+                    View Recipe
+                  </Button>
                 </Card>
-              ))
-              }
+              ))}
             </CardDeck>
           </Jumbotron>
-          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-            <ModalHeader toggle={this.toggle}>{recipeDetails.title}</ModalHeader>
+          <Modal
+            isOpen={this.state.modal}
+            toggle={this.toggle}
+            className={this.props.className}
+          >
+            <ModalHeader toggle={this.toggle}>
+              {recipeDetails.title}
+            </ModalHeader>
             <ModalBody>
-              <img id="myImg" src={recipeDetails.image} alt="recipe"></img>
-              <h1>Cooking time: {recipeDetails.readyInMinutes} Minutes</h1>
-              <h2>Aisle: {aisle}</h2>
+              <img
+                id="myImg"
+                src={recipeDetails.image}
+                width="100%"
+                alt="recipe"
+              />
+              <h6>Prep Time: {recipeDetails.preparationMinutes} Minutes</h6>
+              <h6>Cooking time: {recipeDetails.readyInMinutes} Minutes</h6>
+              <h6>Servings: {recipeDetails.servings}</h6>
+              <h6>Source: {recipeDetails.sourceName}</h6>
+              <h6>Ingredients:</h6>
+              <ul style={{ listStyleType: "none" }}>
+                {ingredients.map(({ id, name }) => (
+                  <li key={id} style={{ listStyleType: "square" }}>
+                    {name}
+                  </li>
+                ))}
+              </ul>
             </ModalBody>
             <Button onClick={this.addToFavs}>Add to Favs</Button>
           </Modal>
