6ef47c402e15f0d61efbf115837436c8094379a7
grantpant <grantpant@gmail.com>
2018-10-25 15:11:15 -0500

    added grid columns to FavRecipe cards

--- client/src/components/FavRecipes.js
+++ client/src/components/FavRecipes.js
@@ -13,7 +13,8 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  ModalFooter
+  ModalFooter,
+  Col
 } from "reactstrap";
 
 class FavRecipes extends Component {
@@ -132,25 +133,26 @@ class FavRecipes extends Component {
         <Container>
           <CardDeck>
             {favRecipes.map(({ _id, image, title }) => (
-              <Card key={_id}>
-              <CardImg top width="100%" src={image} alt="Card image cap" />
-              <CardBody>
-                <CardTitle>{title}</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText></CardText>
-                <Button onClick={this.viewDetails.bind(this, _id)}>View Details</Button>
-
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    style={{ float: "right" }}
-                    onClick={this.deleteFav.bind(this, _id)}
-                  >
-                    &times;
-                  </Button>
-                </CardBody>
-              </Card>
+              <Col key={_id} sm="6">
+                <Card>
+                <CardImg top width="100%" src={image} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>{title}</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText></CardText>
+                  <Button onClick={this.viewDetails.bind(this, _id)}>View Details</Button>
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      style={{ float: "right" }}
+                      onClick={this.deleteFav.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
             ))}
           </CardDeck>
           <Modal isOpen={this.state.modal} toggle={this.toggle}>
