1611c0d81efb260a89828c57c85d63048d9ba851
Curtis Adams <curt.b.adams@gmail.com>
2018-10-28 19:46:01 -0500

    work on random recipe component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import RecipeSearch from './RecipeSearch';
+import React, { Component } from "react";
+import RecipeSearch from "./RecipeSearch";
+import RandomRecipes from "./RandomRecipes";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <RecipeSearch
+          user={this.props.user}
+          logOutUser={this.props.logOutUser}
+        />
+        <h1>What's popular now</h1>
+        <RandomRecipes
           user={this.props.user}
           logOutUser={this.props.logOutUser}
         />
---
+++ client/src/components/RandomRecipes.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import {
+  Container,
+  CardDeck,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Form
+} from "reactstrap";
+//import things here
+
+class RandomRecipes extends Component {
+  state = {
+    randomRecipes: []
+  };
+
+  componentDidMount(err) {
+    console.log("componentWillMount worked");
+    //fired before the render method
+    fetch(
+      `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random`,
+      {
+        method: "GET",
+        headers: {
+          "X-Mashape-Key": "oAClzEfOdWmshwyHDlUeJVmEnmLdp1AKiOIjsnobfNbVPkxYvZ"
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(randomRecipe => {
+        this.setState({ randomRecipes: randomRecipe });
+      })
+      .catch(err);
+  }
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  render() {
+    if (this.state.randomRecipes.recipes) {
+      console.log("it works");
+      var details = this.state.randomRecipes.recipes[0];
+      console.log(details.title);
+    } else {
+      details = [];
+    }
+
+    return (
+      <div className="random-recipe">
+        <Container>
+          <CardDeck>
+            <Card>
+              <CardImg top width="100%" src={details.image} />
+              <CardBody>
+                <CardTitle>{details.title}</CardTitle>
+                <Button>View Recipe</Button>
+              </CardBody>
+            </Card>
+          </CardDeck>
+        </Container>
+      </div>
+    );
+  }
+}
+export default RandomRecipes;
