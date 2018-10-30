561e199b7f3062a438a866b784491ce495a498f3
grantpant <grantpant@gmail.com>
2018-10-29 20:57:18 -0500

    ?

--- client/src/components/randomRecipes.js
+++ client/src/components/randomRecipes.js
@@ -1,105 +1,68 @@
 import React, { Component } from "react";
-import DashNav from "./DashNav";
 import {
   Container,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button,
   CardDeck,
   Card,
   CardImg,
   CardBody,
   CardTitle,
-  Jumbotron
+  CardSubtitle,
+  CardText,
+  Button
 } from "reactstrap";
-//imports for modal
-import { Modal, ModalHeader, ModalBody } from "reactstrap";
+//import things here
 
-class RecipeSearch extends Component {
+class RandomRecipes extends Component {
   state = {
-    searchResults: [],
-    value: "",
-    recipeDetails: {}
+    randomRecipes: []
   };
 
-  componentDidMount() {
-    let userPayload = { user: this.props.user };
-
-    fetch("api/randomRecipes", {
-      method: "GET",
-      body: JSON.stringify(userPayload),
-      headers: { Accept: "application/json" }
-    })
-      .then(res => res.json())
-      .then(data => {
-        let randomRecipes = data.favRecipes;
-        this.setState({ favRecipes });
-      })
-      .catch(err => console.log(err));
-  }
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  // findRecipes = (event, err) => {
-  //   event.preventDefault();
-  //   let searchData = { ingredients: this.state.value };
-  //   // console.log(searchData);
-  //   fetch("api/recipe-search", {
-  //     method: "POST",
-  //     body: JSON.stringify(searchData),
-  //     headers: { "Content-Type": "application/json" }
-  //   })
-  //     .then(function(res) {
-  //       let shit = res.json();
-  //       return shit;
-  //     })
-  //     .then(results => {
-  //       this.setState({ searchResults: results });
-  //     })
-  //     .catch(err);
-  //   this.setState({ value: "" });
-  // };
-  //second api request to get recipe details
-  recipeDetails = (id, event, err) => {
-    event.preventDefault();
-    let recipeId = { idNumber: id };
-    fetch("api/recipe-search/recipedetails", {
-      method: "POST",
-      body: JSON.stringify(recipeId),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(res => res.json())
-      .then(result => {
-        this.setState({ recipeDetails: result });
-        if (this.state.recipeDetails.extendedIngredients) {
-          this.toggle();
+  componentDidMount(err) {
+    console.log("componentWillMount worked");
+    //fired before the render method
+    fetch(
+      `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random`,
+      {
+        method: "GET",
+        headers: {
+          "X-Mashape-Key": "oAClzEfOdWmshwyHDlUeJVmEnmLdp1AKiOIjsnobfNbVPkxYvZ"
         }
+      }
+    )
+      .then(res => res.json())
+      .then(randomRecipe => {
+        this.setState({ randomRecipes: randomRecipe });
       })
       .catch(err);
-    this.setState({ ID: "" });
-  };
+  }
 
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
-
   render() {
-    const { searchResults } = this.state;
-    const { recipeDetails } = this.state;
-    let ingredients;
-    if (this.state.recipeDetails.extendedIngredients) {
-      ingredients = this.state.recipeDetails.extendedIngredients;
-      console.log(ingredients);
+    if (this.state.randomRecipes.recipes) {
+      console.log("it works");
+      var details = this.state.randomRecipes.recipes[0];
+      console.log(details.title);
     } else {
-      ingredients = [];
+      details = [];
     }
 
-    return <div className="search-results" />;
+    return (
+      <div className="random-recipe">
+        <Container>
+          <CardDeck>
+            <Card>
+              <CardImg top width="100%" src={details.image} />
+              <CardBody>
+                <CardTitle>{details.title}</CardTitle>
+                <Button>View Recipe</Button>
+              </CardBody>
+            </Card>
+          </CardDeck>
+        </Container>
+      </div>
+    );
   }
 }
-
-export default RecipeSearch;
+export default RandomRecipes;
