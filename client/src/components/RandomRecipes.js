3910a87f5b9b9fdc43f9dd267e5c01531a6f03ce
Curtis Adams <curtisadams@fb.com>
2018-11-08 16:01:07 -0600

    changes made at work

--- client/src/components/RandomRecipes.js
+++ client/src/components/RandomRecipes.js
@@ -7,13 +7,18 @@ import {
   CardImg,
   CardBody,
   CardTitle,
-  Button
+  Button, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter
 } from "reactstrap";
 //import things here
 
 class RandomRecipes extends Component {
   state = {
-    randomRecipes: []
+    randomRecipes: [], 
+    modal: false
   };
 
   componentDidMount(err) {
@@ -35,6 +40,11 @@ class RandomRecipes extends Component {
       .catch(err);
   }
 
+  viewDetails = _id => {
+    
+    this.toggle();
+  };
+
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
@@ -55,10 +65,20 @@ class RandomRecipes extends Component {
               <CardImg top width="100%" src={details.image} />
               <CardBody>
                 <CardTitle>{details.title}</CardTitle>
-                <Button>View Recipe</Button>
+                <Button onClick={this.viewDetails}>View Recipe</Button>
               </CardBody>
             </Card>
           </CardDeck>
+          <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>{details.title}</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
         </Container>
       </div>
     );
